14c29b05b07a743b6606884cc6b2c9f1470b2206
Micael Duarte <micael.duarte@e-comprei.com>
2023-09-10 00:12:12 -0300

    ensure FacebookApi gets debug token

--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -17,6 +17,7 @@ describe('FacebookApi', () => {
 
   beforeEach(() => {
     sut = new FacebookApi(httpClient, clientId, clientSecret)
+    httpClient.get.mockResolvedValueOnce({ access_token: 'any_app_token' })
   })
 
   it('should get app token', async () => {
@@ -28,6 +29,18 @@ describe('FacebookApi', () => {
         client_id: clientId,
         client_secret: clientSecret,
         grant_type: 'client_credentials'
+      }
+    })
+  })
+
+  it('should get debug token', async () => {
+    await sut.loadUser({ token: 'any_client_token' })
+
+    expect(httpClient.get).toHaveBeenCalledWith({
+      url: 'https://graph.facebook.com/debug_token',
+      params: {
+        access_token: 'any_app_token',
+        input_token: 'any_client_token'
       }
     })
   })
