976a89d6414a554671f17c5051b9769b28678404
Micael Duarte <micael.duarte@e-comprei.com>
2023-09-09 23:01:20 -0300

    ensure AccesToken expires in 30 minutes

--- tests/domain/models/access-token.spec.ts
+++ tests/domain/models/access-token.spec.ts
@@ -4,4 +4,8 @@ describe('AccessToken', () => {
     const sut = new AccessToken('any_value')
     expect(sut).toEqual({ value: 'any_value' })
   })
+
+  it('should expire in 1800000 ms', () => {
+    expect(AccessToken.expirationInMs).toBe(1800000)
+  })
 })
